59903be39ae6bbc27868fd9eb0d4a35e44ea9d22
Siddharth Singh <me@shantaram.xyz>
2023-05-10 17:20:27 +0530

    fix esm

--- src/Input.js
+++ src/Input.js
@@ -54,7 +54,7 @@ Game.Input.keyDownHandler = function (e) {
 
 Game.Input.keyUpHandler = function (e) {
     window.pressedKeys[e.key] = false
-    if (e.key === 'e' && !Game.isPaused()) {
+    if (e.key === 'e' && !Game.isPaused() && Game.Player.trigger) {
         Game.Player.trigger();
     }
     if (e.key === 'Escape') {
