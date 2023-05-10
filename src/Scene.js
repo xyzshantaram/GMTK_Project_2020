137accb655da909d99a44508e86fb9512a6ae7ed
Siddharth Singh <me@shantaram.xyz>
2023-05-10 17:28:59 +0530

    remove scene.js

--- src/Scene.js
+++
@@ -1,9 +0,0 @@
-Game.loadScene = function(jsonStr, callback) {
-    Game.currentScene = JSON.parse(jsonStr);
-    Game.BG = (assets.getAsset(Game.currentScene.BG));
-    Game.Player.spriteObject = new AnimatedSprite();
-    Game.Player.spriteObject.load('mc');
-    Game.setPause(false);
-    window.requestAnimationFrame(draw);
-    callback();
-}
